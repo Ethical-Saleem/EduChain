a98f3d6c2fbf79f31a1bd5bc92a2f314e249f039
Abdussalam <saleemabdussalam404@gmail.com>
2024-07-14 15:44:10 +0100

    added basic site description

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,5 +1,23 @@
 import Layout from "../../../layout/layout";
 import React, { Suspense } from "react";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'EduChain',
+  description: 'Access your school record on the go.',
+  robots: { index: false, follow: false },
+  viewport: { initialScale: 1, width: 'device-width' },
+  openGraph: {
+      type: 'website',
+      title: 'EduChain',
+      url: 'https://edu-chain-one.vercel.app/login',
+      description: 'Access your past school record on the go with relative ease.',
+      ttl: 604800
+  },
+  icons: {
+      icon: '/favicon.ico'
+  }
+};
 
 export default function MainLayout({
   children,
