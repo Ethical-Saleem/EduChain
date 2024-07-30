b96ef8c2403927699e49348dcdff183550610f92
Abdussalam <saleemabdussalam404@gmail.com>
2024-07-30 09:17:37 +0100

    changed apiUrl to deployed aws server

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -1,4 +1,5 @@
 export const config = {
-    apiUrl: "https://edu-chain-services.onrender.com",
+    // apiUrl: "https://edu-chain-services.onrender.com",
+    apiUrl: "http://educhain-repo-balancer-1055466285.eu-north-1.elb.amazonaws.com/"
     // apiUrl: "http://localhost:3001"
 }
